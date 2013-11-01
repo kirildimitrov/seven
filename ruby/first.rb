820d715b0ff7b5c0e076f82df788fa285f7cdb5b
kirildimitrov <ritter@kiri-computer.(none)>
2013-11-01 16:48:00 +0200

    first

---
+++ ruby/first.rb
@@ -0,0 +1,9 @@
+properties= ['object-oriented', 'duck typed', 'productive', 'fun']
+properties.each{|property| puts "Ruby is #{property}."}
+puts 4.class
+x=42
+unless x==4
+  puts 'This apears to be false'
+else
+  puts 'This apears to be True'
+end
